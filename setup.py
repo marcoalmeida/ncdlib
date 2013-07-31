d4250ee4fb4b8df0be198db5e1b9b59a2a98ec9b
Marco Almeida <marcoafalmeida@gmail.com>
2013-07-31 20:59:18 +0100

    Added setup.py for ease of instalation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='ncdlib',
+      maintainer='Marco Almeida',
+      maintainer_email='marcoafalmeida@gmail.com',
+      url='https://github.com/marcoalmeida/ncdlib',
+      version='0.1',
+      py_modules=['ncdlib'],
+      )
